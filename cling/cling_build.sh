34c16436632211404b50d2d8eb244eab4989ca7e
cuihairu <chuihairu@gmail.com>
2019-03-09 06:36:30 +0800

    Update cling_build.sh

--- cling/cling_build.sh
+++ cling/cling_build.sh
@@ -25,12 +25,12 @@ function get_sources() {
 function configure_build() {
     mkdir -p "${BUILD_DIR}"
     cd "${BUILD_DIR}"
-    cmake -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" -DPYTHON_EXECUTABLE=$(which python3) "${SRC_DIR}"
+    cmake3 -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" -DPYTHON_EXECUTABLE=$(which python3) "${SRC_DIR}"
 }
 
 function build_all() {
     cd "${BUILD_DIR}"
-    make3 --jobs=$(nproc) install
+    make3 --jobs=2 install
 }
 
 function cleanup() {
